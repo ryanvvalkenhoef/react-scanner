1d905dcd26dabdf819e74776f4764d1e29e313eb
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-03-18 15:50:03 +0100

    fix: Resolve fetch of config-file

--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@ sade("react-scanner", true)
   .option("-c, --config", "Path to config file")
   .example("-c /path/to/react-scanner.config.js")
   .action((options) => {
-    const configPath = path.resolve(process.cwd(), options.config);
-    const configDir = path.dirname(configPath);
-    const config = require(configPath);
-    run(config, configDir, "cli");
+    const configPath = options.config ? path.resolve(process.cwd(), options.config) : './react-scanner.config.js';
+
+    try {
+      const config = require(configPath);
+      const configDir = path.dirname(configPath);
+      run(config, configDir, "cli");
+    } catch (error) {
+      console.error("Error reading or parsing the configuration file:", error);
+    }
   })
   .parse(process.argv);
